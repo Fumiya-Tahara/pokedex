7afaea3f420fb0c7bfa2b8c763df3b3fd8895127
Fumiya Tahara <taharafumiya0219@gmail.com>
2026-03-22 17:36:24 +0900

    Exec migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_22_000003) do
+  create_table "pokemon_types", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "pokemon_id", null: false
+    t.integer "slot", null: false
+    t.bigint "type_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pokemon_id", "slot"], name: "index_pokemon_types_on_pokemon_id_and_slot", unique: true
+    t.index ["pokemon_id"], name: "index_pokemon_types_on_pokemon_id"
+    t.index ["type_id"], name: "index_pokemon_types_on_type_id"
+  end
+
+  create_table "pokemons", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.decimal "height", precision: 5, scale: 1, null: false
+    t.string "name", null: false
+    t.integer "national_number", null: false
+    t.datetime "updated_at", null: false
+    t.decimal "weight", precision: 7, scale: 1, null: false
+    t.index ["national_number"], name: "index_pokemons_on_national_number", unique: true
+  end
+
+  create_table "types", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_types_on_name", unique: true
+  end
+
+  add_foreign_key "pokemon_types", "pokemons"
+  add_foreign_key "pokemon_types", "types"
 end
