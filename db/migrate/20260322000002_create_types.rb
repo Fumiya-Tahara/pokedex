b8526a86b44b363c3acb8ab2eb025c1443f5d264
Fumiya Tahara <taharafumiya0219@gmail.com>
2026-03-22 17:32:26 +0900

    Add migration files

---
+++ db/migrate/20260322000002_create_types.rb
@@ -0,0 +1,11 @@
+class CreateTypes < ActiveRecord::Migration[8.1]
+  def change
+    create_table :types do |t|
+      t.string :name, null: false
+
+      t.timestamps
+    end
+
+    add_index :types, :name, unique: true
+  end
+end
